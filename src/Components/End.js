9a5aa84a3fa1cd6b6d18294b6815817e42fa0fba
Brandon <brandonstrash91@yahoo.com>
2019-03-21 22:30:49 -0400

    added keyboard navigation for game over and game modes

--- src/Components/End.js
+++ src/Components/End.js
@@ -1,27 +1,35 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect, useRef } from "react";
 import Logo from "./Logo";
+export default ({ history, ...props }) => {
+  const [cursor, setCursor] = useState(0);
+  const ul = useRef(null);
+  const handleKeyDown = e => {
+    e.preventDefault();
+    const { time, players } = props.match.params;
+    console.log(e.keyCode);
+    // arrow up/down button should select next/previous list element
+    if (e.keyCode === 38 || e.keyCode === 40) {
+      setCursor((cursor + 1) % 2);
+    } else if (e.keyCode === 13) {
+      if (cursor == 0) {
+        history.push(`/game-play/${time}/${players}`);
+      } else {
+        history.push(`/game-modes`);
+      }
+    }
+  };
+  useEffect(() => {
+    ul.current.focus();
+  }, [ul]);
 
-class End extends Component {
-  render() {
-    return (
-      <>
-        <Logo />
-        <h3>Game Over</h3>
-        <Link
-          to={
-            "/game-play/" +
-            this.props.match.params.time +
-            "/" +
-            this.props.match.params.players
-          }
-        >
-          Retry
-        </Link>
-        <Link to="/game-modes">Home</Link>
-      </>
-    );
-  }
-}
-
-export default End;
+  return (
+    <div className="container">
+      <Logo />
+      <h3>Game Over</h3>
+      <ul ref={ul} tabIndex="0" className="players" onKeyDown={handleKeyDown}>
+        <li className={cursor === 0 && "selected"}>Retry</li>
+        <li className={cursor === 1 && "selected"}>Home</li>
+      </ul>
+    </div>
+  );
+};
--- src/Components/GameMode.js
+++ src/Components/GameMode.js
@@ -1,18 +1,16 @@
 import React, { useState, useEffect, useRef } from "react";
 import Logo from "./Logo";
 export default ({ history }) => {
-  const [cursor, setCursor] = useState(1);
+  const [cursor, setCursor] = useState(0);
   const ul = useRef(null);
   const handleKeyDown = e => {
     e.preventDefault();
     console.log(e.keyCode);
     // arrow up/down button should select next/previous list element
-    if (e.keyCode === 38 && cursor > 1) {
-      setCursor(cursor - 1);
-    } else if (e.keyCode === 40 && cursor < 2) {
-      setCursor(cursor + 1);
+    if (e.keyCode === 38 || e.keyCode === 40) {
+      setCursor((cursor + 1) % 2);
     } else if (e.keyCode === 13) {
-      history.push(`/game-play/5/${cursor}`);
+      history.push(`/game-play/5/${cursor + 1}`);
     }
   };
   useEffect(() => {
@@ -24,8 +22,8 @@ export default ({ history }) => {
       <Logo />
       <h3>Players</h3>
       <ul ref={ul} tabIndex="0" className="players" onKeyDown={handleKeyDown}>
-        <li className={cursor === 1 && "selected"}>1P</li>
-        <li className={cursor === 2 && "selected"}>2P</li>
+        <li className={cursor === 0 && "selected"}>1P</li>
+        <li className={cursor === 1 && "selected"}>2P</li>
       </ul>
     </div>
   );
