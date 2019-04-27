25635248aeaec528228c36d44a17a25f5a3177b5
Brandon <brandonstrash91@yahoo.com>
2019-04-27 18:31:39 -0400

    fix home audio

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,41 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import { useAudio } from 'react-use';
+import React from 'react';
 import introMusic from '../sounds/1Intro.wav';
 import music from '../sounds/2 Start Screen.wav';
 import basketball from '../img/Basketball.png';
 import logo from '../img/Logo.png';
-export default ({ history }) => {
-  const [go, setGo] = useState(false);
-  // const audioRef = useRef();
-  useEffect(() => {
+import soundContext from '../utilities/soundContext';
+export default class Home extends React.Component {
+  state = {
+    go: false
+  };
+  componentDidMount() {
+    const musicObj = new Audio(introMusic);
+    musicObj.play();
+    musicObj.onended = this.startNextTrack(musicObj);
     setTimeout(() => {
-      setGo(true);
+      this.setState({ go: true });
     }, 1000);
-  }, [go]);
-  const startNextTrack = () => {
-    controls.play();
-  };
-  const [introAudio] = useAudio({
-    src: introMusic,
-    autoPlay: true,
-    onEnded: startNextTrack
-  });
-  // eslint-disable-next-line no-unused-vars
-  const [musicAudio, state, controls] = useAudio({
-    src: music,
-    autoPlay: false,
-    loop: true,
-    onEnded: startNextTrack
-  });
+  }
 
-  const navigate = () => {
-    history.push('/game/game-modes');
+  startNextTrack = prevMusic => () => {
+    prevMusic.pause();
+    var url = music;
+
+    /* --- set up web audio --- */
+    //...and the source
+    var source = soundContext.createBufferSource();
+    //connect it to the destination so you can hear it.
+    source.connect(soundContext.destination);
+
+    /* --- load buffer ---  */
+    var request = new XMLHttpRequest();
+    //open the request
+    request.open('GET', url, true);
+    //webaudio paramaters
+    request.responseType = 'arraybuffer';
+    //Once the request has completed... do this
+    request.onload = function() {
+      soundContext.decodeAudioData(
+        request.response,
+        function(response) {
+          /* --- play the sound AFTER the buffer loaded --- */
+          //set the buffer to the response we just received.
+          source.buffer = response;
+          //start(0) should play asap.
+          source.start(0);
+          source.loop = true;
+        },
+        function() {
+          console.error('The request failed.');
+        }
+      );
+    };
+    //Now that the request has been defined, actually make the request. (send it)
+    request.send();
+    this.setState({ music: source });
   };
-  return (
-    <>
-      {introAudio}
-      {musicAudio}
-      <div className="container" tabIndex="0" onKeyDown={navigate}>
+
+  navigate = () => {
+    this.props.history.push('/game/game-modes');
+  };
+  componentWillUnmount() {
+    if (this.state.music) {
+      this.state.music.stop();
+    }
+  }
+  render() {
+    const { go } = this.state;
+    return (
+      <div
+        autoFocus
+        className="container"
+        tabIndex="0"
+        onKeyDown={this.navigate}
+      >
         <div className={go ? 'beth on' : 'beth off'} />
         <div className={go ? 'travis on' : 'travis off'} />
         <img className="logo-big" src={logo} alt="logo" />
@@ -45,6 +81,6 @@ export default ({ history }) => {
           <img alt="basketball" src={basketball} />
         </div>
       </div>
-    </>
-  );
-};
+    );
+  }
+}
